fe6b9816b51f10e5f98fc677f629b61bf4bbb337
Mr-Evgen-E <you@example.comeev-evgeny@yandex.ru>
2024-02-25 21:28:19 +0300

    add new tests

--- tests/first_test.py
+++ tests/first_test.py
@@ -2,11 +2,19 @@ import pytest
 
 from app.calc import Calculator
 
+
 class TestCalc:
     def setup(self):
         self.calc = Calculator
 
-    def test_adding_success(self):
-        assert self.calc.adding(self,2, 1) == 3
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
 
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 6, 2) == 3
 
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 10, 3) == 7
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 2, 1) == 3
