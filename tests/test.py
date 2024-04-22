eb9e846b5d2340bd337fe4074cac0ce0b3e94e6d
Evgeny <eev-evgeny@yandex.ru>
2024-04-22 17:16:27 +0700

    Final

--- tests/test.py
+++ tests/test.py
@@ -8,13 +8,20 @@ class TestCalc:
         self.calc = Calculator
 
     def test_multiply_calculate_correctly(self):
+        # Позитивный тест умножения числа на число
         assert self.calc.multiply(self, 2, 2) == 4
 
     def test_division_calculate_correctly(self):
+        # Позитивный тест деления числа на число
         assert self.calc.division(self, 6, 2) == 3
 
     def test_subtraction_calculate_correctly(self):
+        # Позитивный тест вычитания одного числа из другого
         assert self.calc.subtraction(self, 10, 3) == 7
 
     def test_adding_calculate_correctly(self):
+        # Позитивный тест сложения двух чисел
         assert self.calc.adding(self, 2, 1) == 3
+
+    def teardown(self):
+        print("Выполнение метода Teardown")
